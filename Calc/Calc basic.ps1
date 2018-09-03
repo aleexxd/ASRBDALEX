668643fe91d246f478e436415651ae5963d094b3
aleexxd <alex.deroin@wanadoo.fr>
2018-09-03 14:51:24 +0200

    Calculette v0

---
+++ Calc/Calc basic.ps1
@@ -0,0 +1,41 @@
+﻿<# 
+
+            Programmation Calculatrice
+
+
+#>
+
+Echo " Bienvenue dans la calculatrice"
+
+While ($true) {
+
+[INT]$number1 = Read-Host " Première Valeur "
+
+Write-Host 
+"
+######################
+#                    #
+# + (Addition)       #
+# - (Soustraction)   #
+# / (Division)       #
+# x (Multiplication) #
+#                    #
+######################"
+
+$opération = Read-Host " Quel est l'opération ? ( + , - , / , x )"
+
+
+[INT]$number2 = Read-Host " Deuxième Valeur "
+
+
+$resultat = Switch ( $opération )
+{
+    "+" { $number1 + $number2 }
+    "-" { $number1 - $number2 }
+    "/" { $number1 / $number2 }
+    "x" { $number1 * $number2 }
+    default { "ERROR CHAISE CLAVIER" }
+}
+
+Write-Host " Resultat : $resultat "
+}
---
+++ Calc/Calc.ps1
@@ -0,0 +1,54 @@
+﻿<# 
+
+            Programmation Calculatrice
+
+
+#>
+
+Echo " Bienvenue dans la calculatrice"
+
+
+#While Permet de faire une boucle infinie
+
+While ($true) {
+
+#Demander a saisir la Première Valeur
+
+[INT]$number1 = Read-Host " Première Valeur "
+
+#Tableau pour montrer les choix disponibles
+
+Write-Host 
+"
+######################
+#                    #
+# + (Addition)       #
+# - (Soustraction)   #
+# / (Division)       #
+# x (Multiplication) #
+#                    #
+######################"
+
+#Demande quel opération la calculette doit effectuer
+
+$opération = Read-Host " Quel est l'opération ? ( + , - , / , x )"
+
+#La deuxieme valeur pour le calcul
+
+[INT]$number2 = Read-Host " Deuxième Valeur "
+
+#Calcul choix multiple en fonction de la réponse a la question pour opération
+
+$resultat = Switch ( $opération )
+{
+    "+" { $number1 + $number2 }
+    "-" { $number1 - $number2 }
+    "/" { $number1 / $number2 }
+    "x" { $number1 * $number2 }
+    default { "ERROR CHAISE CLAVIER" }
+}
+
+#On affiche la valeur a l'écran
+
+Write-Host " Resultat : $resultat "
+}
